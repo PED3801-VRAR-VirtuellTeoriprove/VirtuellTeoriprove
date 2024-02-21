68f054007560f4bce99cf8a055a608031bd92d43
Christian Le <crezle1999@gmail.com>
2024-02-21 11:47:51 +0100

    complete 'get in car' functionality

--- Unity/Assets/Scripts/XRCarClick.cs
+++ Unity/Assets/Scripts/XRCarClick.cs
@@ -9,12 +9,8 @@ using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
 public class XRCarClick : MonoBehaviour
 {
     private XRSimpleInteractable interactable;
-    public Transform perspective;
-    public Transform leftController;
-    public Transform leftControllerStabilizer;
-    public Transform rightController;
-    public Transform rightControllerStabilizer;
-    public Transform gazeInteractor;
+    public Transform bodyOrigin;
+    public DynamicMoveProvider bodyMove;
     public Transform driverSeat;
 
     void Start()
@@ -25,40 +21,23 @@ public class XRCarClick : MonoBehaviour
 
     void HandleSelectEntered(SelectEnterEventArgs args)
     {
-        Vector3 cameraLocalPosition = perspective.localPosition;
-        Quaternion cameraLocalRotation = perspective.localRotation;
-        Vector3 leftControllerLocalPosition = leftController.localPosition;
-        Quaternion leftControllerLocalRotation = leftController.localRotation;
-        Vector3 rightControllerLocalPosition = rightController.localPosition;
-        Quaternion rightControllerLocalRotation = rightController.localRotation;
-        Vector3 leftStabilizerLocalPosition = leftControllerStabilizer.localPosition;
-        Quaternion leftStabilizerLocalRotation = leftControllerStabilizer.localRotation;
-        Vector3 rightStabilizerLocalPosition = rightControllerStabilizer.localPosition;
-        Quaternion rightStabilizerLocalRotation = rightControllerStabilizer.localRotation;
-        Vector3 gazeInteractorLocalPosition = gazeInteractor.localPosition;
-        Quaternion gazeInteractorLocalRotation = gazeInteractor.localRotation;
+        bodyOrigin.position = driverSeat.position;
+        bodyOrigin.rotation = driverSeat.rotation;
 
-        // Move the camera to the driver's seat of the car
-        perspective.position = driverSeat.position;
-        perspective.rotation = driverSeat.rotation;
+        ResetChildren(bodyOrigin.gameObject);
 
-        // Move the controllers, stabilizers and gaze interactor to their original local positions and rotations relative to the camera
-        leftController.position = perspective.TransformPoint(cameraLocalPosition + leftControllerLocalPosition);
-        leftController.rotation = perspective.rotation * leftControllerLocalRotation;
-        rightController.position = perspective.TransformPoint(cameraLocalPosition + rightControllerLocalPosition);
-        rightController.rotation = perspective.rotation * rightControllerLocalRotation;
-        leftControllerStabilizer.position = perspective.TransformPoint(cameraLocalPosition + leftStabilizerLocalPosition);
-        leftControllerStabilizer.rotation = perspective.rotation * leftStabilizerLocalRotation;
-        rightControllerStabilizer.position = perspective.TransformPoint(cameraLocalPosition + rightStabilizerLocalPosition);
-        rightControllerStabilizer.rotation = perspective.rotation * rightStabilizerLocalRotation;
-        gazeInteractor.position = perspective.TransformPoint(cameraLocalPosition + gazeInteractorLocalPosition);
-        gazeInteractor.rotation = perspective.rotation * gazeInteractorLocalRotation;
-
-        // Disable the camera movement
-        perspective.GetComponentInChildren<DynamicMoveProvider>(true).enabled = false;
-
+        bodyMove.enabled = false;
         // Code to handle "click" event
         Debug.Log("Car was clicked (selected)");
+    }
+
+    void ResetChildren(GameObject gameObject)
+    {
+        foreach (Transform child in gameObject.transform)
+        {
+            child.localPosition = Vector3.zero;
+            child.gameObject.SetActive(true);
+        }
     }
     void OnDestroy()
     {
