b099aacbbdbc856c71832d754eb5df61458eec43
haralg <haralbbj@stud.ntnu.no>
2024-02-21 10:08:58 +0100

    Toggle multiple choice panel

--- Unity/Assets/Scripts/MultipleChoiceMenu.cs
+++ Unity/Assets/Scripts/MultipleChoiceMenu.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TogglePanel : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Button toggleButton;
+    public GameObject panel;
     void Start()
     {
-        
+        toggleButton.onClick.AddListener(TogglePanelVisibility);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void TogglePanelVisibility()
+    {
+        panel.SetActive(!panel.activeSelf);
     }
 }
