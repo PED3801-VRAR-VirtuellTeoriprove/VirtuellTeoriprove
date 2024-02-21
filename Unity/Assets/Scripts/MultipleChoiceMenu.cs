528fe30ba41d7a2a73df1e414c23f8476f44eb3a
haralg <haralbbj@stud.ntnu.no>
2024-02-21 13:50:21 +0100

    Multiple Choice menu changes text areas when passing through event trigger. Some logic for answer choice.

--- Unity/Assets/Scripts/MultipleChoiceMenu.cs
+++ Unity/Assets/Scripts/MultipleChoiceMenu.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TogglePanel : MonoBehaviour
+public class MultipleChoiceMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
     public Button toggleButton;
+    public Button[] answerButtons;
     public GameObject panel;
+    public TMP_Text questionText;
+    public int correctAnswer;
+
     void Start()
     {
         toggleButton.onClick.AddListener(TogglePanelVisibility);
+        for (int i = 0; i < 4; i++)
+        {
+            answerButtons[i].onClick.AddListener(() => CheckAnswer(i));
+        }
     }
 
     // Update is called once per frame
@@ -22,5 +30,40 @@ public class TogglePanel : MonoBehaviour
     void TogglePanelVisibility()
     {
         panel.SetActive(!panel.activeSelf);
+    }
+
+    public void SetQuestion(string question)
+    {
+        questionText.text = question;
+    }
+
+    public void SetAnswers(string[] answers)
+    {
+        if (answers.Length != 4)
+        {
+            Debug.LogError("Answers array must have 4 elements");
+            return;
+        }
+        for (int i = 0; i < 4; i++)
+        {
+            answerButtons[i].GetComponentInChildren<TMP_Text>().text = answers[i];
+        }
+    }
+
+    public void SetCorrectAnswer(int correct)
+    {
+        correctAnswer = correct;
+    }
+
+    void CheckAnswer(int answer)
+    {
+        if (answer == correctAnswer)
+        {
+            Debug.Log("Correct Answer");
+        }
+        else
+        {
+            Debug.Log("Wrong Answer");
+        }
     }
 }
--- Unity/Assets/Scripts/PlayerScript.cs
+++ Unity/Assets/Scripts/PlayerScript.cs
@@ -6,10 +6,12 @@ public class PlayerScript : MonoBehaviour
 {
     // Get UI Multiple Choice Canvas object
     public GameObject multipleChoiceCanvas;
+    public int score;
     void Start()
     {
         // Hide the Multiple Choice Canvas
         multipleChoiceCanvas.SetActive(false);
+        score = 0;
     }
 
     // Update is called once per frame
@@ -24,9 +26,20 @@ public class PlayerScript : MonoBehaviour
         // If the player enters the trigger area
         if (other.gameObject.tag is "TriggerPlane1")
         {
+            MultipleChoiceMenu canvasScript = multipleChoiceCanvas.GetComponent<MultipleChoiceMenu>();
             // Show the Multiple Choice Canvas
             other.gameObject.SetActive(false);
             multipleChoiceCanvas.SetActive(true);
+
+            canvasScript.SetQuestion("What is the capital of France?");
+            canvasScript.SetAnswers(new string[] {"Paris", "London", "Berlin", "Madrid"});
+            canvasScript.SetCorrectAnswer(0);
         }
+    }
+
+    public void CorrectAnswer()
+    {
+        Debug.Log("Correct Answer");
+        score++;
     }
 }
