d609170ec25223239c81803ab6ceb162e09553c2
Christian Le <crezle1999@gmail.com>
2024-04-03 14:38:42 +0200

    add car movement to rundkjoring

--- Unity/Assets/Scripts/CarControl.cs
+++ Unity/Assets/Scripts/CarControl.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.ProBuilder.MeshOperations;
 
 public class CarControl : MonoBehaviour
 {
     private int state = 0;
-    private bool move_active = false;
-    private bool actionIsActive = false;
+    private bool moveActive = false;
+    private bool pivotIsActive = false;
     private float startTime;
     private float t;
     private float lastRotation;
     private Vector3 velocity = Vector3.zero;
+    private int recentActionIndex;
 
     [System.Serializable]
     public class MovementProperties
@@ -31,29 +33,30 @@ public class CarControl : MonoBehaviour
     public List<Action> action_sequences;
     void Update()
     {
-        if (move_active && state < action_sequences.Count) {
+        if (moveActive && state < action_sequences.Count) {
             t = Time.time - startTime;
             List<MovementProperties> currentMovesProps = action_sequences[state].movementProperties;
 
             float timePassed = 0;
             for (int i = 0; i < currentMovesProps.Count; i++) {
+                // Create timeline w/ timestamps for each action
+                // If an action is within current time, execute it
                 float actionStartTime = timePassed;
                 float actionEndTime = timePassed + currentMovesProps[i].duration;
                 timePassed = actionEndTime;
 
-                // Skip this iteration if the transform is null or standby is true
-                if (currentMovesProps[i].standby == true || currentMovesProps[i].destination == null) {
-                    continue;
-                }
-
                 if (t > actionStartTime && t <= actionEndTime) {
                     Transform targetPos = currentMovesProps[i].destination;
                     float actionProgress = (t - actionStartTime) / currentMovesProps[i].duration;
-                    if (currentMovesProps[i].isPivot) {
-                        if (!actionIsActive) {
+                    if (currentMovesProps[i].standby || currentMovesProps[i].destination == null) {
+                        // Skip this iteration if the transform is null or standby is true
+                        continue;
+                    }
+                    else if (currentMovesProps[i].isPivot) {
+                        if (!pivotIsActive || recentActionIndex != i) {
                             Debug.Log("Action is active, resetting lastRotation");
                             lastRotation = 0;
-                            actionIsActive = true;
+                            pivotIsActive = true;
                         }
                         float rotationSoFar = currentMovesProps[i].degrees * actionProgress;
                         float rotationThisFrame = rotationSoFar - lastRotation;
@@ -65,35 +68,31 @@ public class CarControl : MonoBehaviour
                         if (direction > 0) {
                             // Pivot is to the right, rotate clockwise
                             transform.RotateAround(targetPos.position, Vector3.up, rotationThisFrame);
+                            Debug.Log("Rotating clockwise");
                         } 
                         else if (direction < 0) {
                             // Pivot is to the left, rotate counterclockwise
                             transform.RotateAround(targetPos.position, Vector3.up, -rotationThisFrame);
+                            Debug.Log("Rotating counterclockwise");
                         }
                         else {
                             Debug.Log("Pivot is directly behind the car");
                         }
 
                         transform.rotation = Quaternion.LookRotation(transform.forward, transform.up);
-                    } 
+                        Debug.Log("");
+                    }
                     else {
-                        if (!actionIsActive) {
-                            Debug.Log("Action is active"); // NOTE: This is printed too many times
-                            // velocity = Vector3.zero;
-                            actionIsActive = true;
-                        }
+                        pivotIsActive = false;
                         float smoothTime = currentMovesProps[i].duration / 2;
                         transform.position = Vector3.SmoothDamp(transform.position, targetPos.position, ref velocity, smoothTime);
                     }
-                }
-                else if (t > actionEndTime) {
-                    Debug.Log("Action is not active");
-                    actionIsActive = false;
+                    recentActionIndex = i;
                 }
             }
 
             if (t > timePassed){
-                move_active = false;
+                moveActive = false;
                 if (state < action_sequences.Count) {
                     state++;
                 }
@@ -106,7 +105,7 @@ public class CarControl : MonoBehaviour
     }
     public void MoveToNextQ() {
         if (state < action_sequences.Count) {
-            move_active = true;
+            moveActive = true;
             startTime = Time.time;
         }
     }
