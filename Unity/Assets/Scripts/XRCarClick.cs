86ac1e22f7fd53d653eb66d635cdeb795ec41b8e
Christian Le <crezle1999@gmail.com>
2024-04-16 13:29:00 +0200

    update scenario1

--- Unity/Assets/Scripts/XRCarClick.cs
+++ Unity/Assets/Scripts/XRCarClick.cs
@@ -5,6 +5,7 @@ using Unity.XR.CoreUtils;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Inputs;
+using UnityEngine.XR.Interaction.Toolkit.Inputs.Simulation;
 using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
 
 public class XRCarClick : MonoBehaviour
@@ -14,15 +15,21 @@ public class XRCarClick : MonoBehaviour
     public XRBaseInteractable carTeleportAnchor;
     public XRBaseInteractor teleportInteractor;
     public XRRayInteractor rayInteractor;
-    public GameObject driverSeat;
+    public Transform driverSeat;
+    public Transform driverSeatSim;
     public GameObject startMenu;
     public GameObject carBody;
+    public XRDeviceSimulator XR_DeviceSimulator;
     private MeshCollider bodyCollider;
 
     void Start()
     {
         carTeleportAnchor.selectEntered.AddListener(HandleSelectEntered);
         bodyCollider = carBody.GetComponent<MeshCollider>();
+        if (XR_DeviceSimulator.isActiveAndEnabled)
+        {
+            driverSeat.position = driverSeatSim.position;
+        }
     }
 
     private void HandleSelectEntered(SelectEnterEventArgs args)
@@ -32,9 +39,8 @@ public class XRCarClick : MonoBehaviour
             //Make the XR Rig a child of the car
             xrorigin.transform.parent = carTeleportAnchor.transform;
 
-            UnityEngine.Vector3 offset = xrorigin.transform.position - cameraOffset.transform.position;
-            xrorigin.transform.position = driverSeat.transform.position + offset;
-            xrorigin.transform.rotation = driverSeat.transform.rotation;
+            xrorigin.transform.position = driverSeat.position;
+            xrorigin.transform.rotation = driverSeat.rotation;
 
             teleportInteractor.enabled = false;
             carTeleportAnchor.enabled = false;
