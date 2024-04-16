fed0f6754049799a21a188d6651fac5e20d55d62
haralg <haralbbj@stud.ntnu.no>
2024-04-16 17:01:49 +0200

    Fix scenario 1

--- Unity/Assets/Scripts/PlayerScript.cs
+++ Unity/Assets/Scripts/PlayerScript.cs
@@ -1,34 +1,40 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using Unity.XR.CoreUtils;
-using UnityEditor.PackageManager;
-using UnityEditor.UIElements;
 using UnityEngine;
-using UnityEngine.Analytics;
-using UnityEngine.Events;
-using UnityEngine.InputSystem.UI;
+using UnityEngine.UI;
 
 [Serializable]
 public class Answer
 {
-    public string text;
+    public String text;
+    public bool isCorrect;
     public Sprite image;
+    public String feedback;
 }
 [Serializable]
 public class TheoryProblem
 {
-    public GameObject triggerPlane;
     public String question;
+    public GameObject triggerPlane;
     public Answer[] answers;
-    public int correctAnswer;
-    public String wrongAnswerFeedback;
-    public String correctAnswerFeedback;
 }
 
 public class PlayerScript : MonoBehaviour
 {
+    public GameObject qCanvas;
+    
+    public GameObject[] answerButtons;
+    private TMP_Text questionText;
+
+    public GameObject feedbackPanelCorrect;
+    private TMP_Text feedbackTextCorrect;
+
+    public GameObject feedbackPanelWrong;
+    private TMP_Text feedbackTextWrong;
+
+    public TheoryProblem[] problems;
+    
     private TheoryProblem get_problem(GameObject triggerPlane)
     {
         for (int i = 0; i < problems.Length; i++)
@@ -40,41 +46,15 @@ public class PlayerScript : MonoBehaviour
         }
         return null; // TODO: Throw or return error
     }
-    
-    // Get UI Multiple Choice Canvas object
-    private MultipleChoiceMenu canvasScript;
-    // public GameObject multipleChoiceMenu;
-    public GameObject qCanvas;
-    public TMP_Text scoreNumber;
-    public int score;
-
-    public GameObject feedbackPanelCorrect;
-    private TMP_Text feedbackTextCorrect;
-
-    public GameObject feedbackPanelWrong;
-    private TMP_Text feedbackTextWrong;
-
-    public TheoryProblem[] problems;
 
     void Start()
     {
         // Hide the Multiple Choice Canvas
         qCanvas.SetActive(false);
-
-        qCanvas.GetComponent<MultipleChoiceMenu>().correctAnswerEvent.AddListener(CorrectAnswer);
-        qCanvas.GetComponent<MultipleChoiceMenu>().wrongAnswerEvent.AddListener(WrongAnswer);
+        questionText = qCanvas.GetNamedChild("Question").GetNamedChild("qText").GetComponent<TMP_Text>();
 
         feedbackTextCorrect = feedbackPanelCorrect.GetNamedChild("Text").GetComponent<TMP_Text>();
         feedbackTextWrong = feedbackPanelWrong.GetNamedChild("Text").GetComponent<TMP_Text>();
-        
-        canvasScript = qCanvas.GetComponent<MultipleChoiceMenu>();
-        score = 0;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     private void OnTriggerEnter(Collider other)
@@ -83,35 +63,40 @@ public class PlayerScript : MonoBehaviour
         var problem = get_problem(other.gameObject);
         if (problem != null)
         {
+            
+            if (problem.answers.Length != 4)
+            {
+                Debug.LogError("Answers array must have 4 elements");
+                return;
+            }
             qCanvas.SetActive(true);
-            canvasScript.SetQuestion(problem.question);
-            canvasScript.SetAnswers(problem.answers);
-            canvasScript.SetCorrectAnswer(problem.correctAnswer);
-
-            feedbackTextCorrect.text = problem.correctAnswerFeedback;
-            feedbackTextWrong.text = problem.wrongAnswerFeedback;
+            questionText.text = problem.question;
+            
+            for (int i = 0; i < problem.answers.Length; ++i)
+            {
+                Answer curAnswer = problem.answers[i];
+                answerButtons[i].GetNamedChild("Text").GetComponent<TMP_Text>().text = curAnswer.text;
+                answerButtons[i].GetNamedChild("Image").GetComponent<Image>().sprite = curAnswer.image;
+                answerButtons[i].GetComponent<Button>().onClick.AddListener(delegate { CheckAnswer(curAnswer); });
+            }
         }
-        
     }
 
-    public void CorrectAnswer()
+    public void CheckAnswer(Answer answer)
     {
-        Debug.Log("From player: Correct Answer");
-        qCanvas.SetActive(false);
-        score++;
-        
-        scoreNumber.text = "" + score;
-        
-        feedbackPanelCorrect.SetActive(true);
-
-    }
-
-    public void WrongAnswer()
-    {
-        // TODO: Do some instructive shit
-        Debug.Log("From player: Wrong Answer");
         qCanvas.SetActive(false);
 
-        feedbackPanelWrong.SetActive(true);
+        if (answer.isCorrect)
+        {
+            Debug.Log("From player: Correct Answer");
+            feedbackTextCorrect.text = answer.feedback;
+            feedbackPanelCorrect.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("From player: Incorrect Answer");
+            feedbackTextWrong.text = answer.feedback;
+            feedbackPanelWrong.SetActive(true);
+        }
     }
 }
